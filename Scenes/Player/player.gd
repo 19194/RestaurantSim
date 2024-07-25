76597bdf3bc90865eb840d5995ebf84020eb202c
19194 <19194@jpc.school.nz>
2024-07-25 12:39:03 +1200

    velocity is changed according to direction and speed

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var move_speed : float = 100
+@export var move_speed : float = 100 # the move speed of character, is able to be edited from inspector
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,7 +9,7 @@ func _ready():
 
 func _physics_process(delta):
 	# variable that stores direction of player from input
-	var direction = (Input.get_action_strength("right") - Input.get_action_strength("left"))
+	var input_direction = (Input.get_action_strength("right") - Input.get_action_strength("left"))
 	
-	print(direction)
+	velocity = input_direction * move_speed
 	
