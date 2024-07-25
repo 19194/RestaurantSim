ffb1ed0873b1640ff7975fe9783a03042f6554bb
Mickey <91348503+WickeyMu@users.noreply.github.com>
2024-07-25 19:31:39 +1200

    players animation changes direction based on movement
    made a new function which updates the direction when the player inputs a direction

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -13,7 +13,14 @@ func _physics_process(delta):
 	# variable that stores direction of player from input
 	var input_direction = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"), 0)
 	
+	update_animation_tree(input_direction)
+	
 	velocity = input_direction * move_speed
 	
 	move_and_slide()
 	
+	
+func update_animation_tree(move_input : Vector2):
+	if (move_input != Vector2.ZERO): # if the player is moving
+		animation_tree.set("parameters/Walk/blend_position", move_input) # set the animation in the direction of movement for walk
+		#animation_tree.set("parameters/Idle/blend_position", move_input) # set the animation in the direction of movement for idle
