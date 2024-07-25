b86232eb43047b85a825accf1990ea2a2256c423
Mickey <91348503+WickeyMu@users.noreply.github.com>
2024-07-25 19:40:38 +1200

    function that makes player switch between walking and idle

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 @onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
 
-var move_speed : float = 100
+var move_speed : float = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,14 +14,20 @@ func _physics_process(delta):
 	# variable that stores direction of player from input
 	var input_direction = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"), 0)
 	
-	update_animation_tree(input_direction)
-	
 	velocity = input_direction * move_speed
 	
 	move_and_slide()
+	update_animation_tree(input_direction)
+	update_animation_state()
 	
 	
 func update_animation_tree(move_input : Vector2):
 	if (move_input != Vector2.ZERO): # if the player is moving
 		animation_tree.set("parameters/Walk/blend_position", move_input) # set the animation in the direction of movement for walk
-		#animation_tree.set("parameters/Idle/blend_position", move_input) # set the animation in the direction of movement for idle
+		animation_tree.set("parameters/Idle/blend_position", move_input) # set the animation in the direction of movement for idle
+		
+func update_animation_state():
+	if (velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	elif (velocity == Vector2.ZERO):
+		state_machine.travel("Idle")
