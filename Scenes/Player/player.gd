c03ae0cd16ccc158c78005c6e838f1baf08e1ce4
19194 <19194@jpc.school.nz>
2024-07-25 13:04:40 +1200

    made player able to move

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,15 +1,14 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100 # the move speed of character, is able to be edited from inspector
+	
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	# variable that stores direction of player from input
 	var input_direction = (Input.get_action_strength("right") - Input.get_action_strength("left"))
 	
-	velocity = input_direction * move_speed
+	velocity = input_direction * move_speed # velocity is equal to the direction and move speed
+	
+	move_and_slide()
+	
 	
