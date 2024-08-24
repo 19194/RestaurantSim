f84c7b0ef76a2db87df11231496c75c16190ffcf
Mickey <91348503+WickeyMu@users.noreply.github.com>
2024-08-25 10:10:02 +1200

    test for signals work

--- Scenes/main/main.gd
+++ Scenes/main/main.gd
@@ -3,9 +3,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	SignalScript.upgrade_button.connect(upgrade_button_pressed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+	
+func upgrade_button_pressed():
+	print("upgrade")
