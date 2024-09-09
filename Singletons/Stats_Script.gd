886256cae79dd9020b828d0bbc570c35c0b1cd09
Mickey <DumdaMage369@gmail.com>
2024-09-09 12:05:36 +1200

    About to code upgrade menu
    Added stats script
    Made cost labels

---
+++ Singletons/Stats_Script.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# Player Stats
+var money : int = 0
+var time : int = 0
+var day : int = 0
+var satisfaction : int = 0
+
+# Game Stats
+var furniture_cost = [100, 200, 350]
+var wall_cost = [100, 150, 200]
+var kitchen_cost = [200, 400, 600]
+
+var furniture_effect = [10, 20, 40, 70]
+var wall_effect = [10, 20, 40, 70]
+var kitchen_effect = [10, 20, 40, 70]
