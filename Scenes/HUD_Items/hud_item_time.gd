0659486e040c1f65766d482903a0f7d6c00e0982
Mickey <DumdaMage369@gmail.com>
2024-09-27 10:25:41 +1200

    made the time  change every minute

--- Scenes/HUD_Items/hud_item_time.gd
+++ Scenes/HUD_Items/hud_item_time.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 
 	
 func _physics_process(delta: float) -> void:
-	total_time += delta * 2
+	total_time += delta / 60
 	
 	var time = int(total_time)
 	StatsScript.time = time
